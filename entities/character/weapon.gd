fbc54621b315d19236c6e13172959d37f2341622
Dodoveloper <davide.cor94@gmail.com>
2023-06-24 19:20:02 +0200

    feat: added crosshair and enemies, plus weapon collision detection

--- entities/character/weapon.gd
+++ entities/character/weapon.gd
@@ -4,11 +4,13 @@ extends Node3D
 @export var fire_rate := 0.08
 @export var mag_size := 30
 @export var reload_time := 1.2
+@export var raycast_path: NodePath
 
 var cur_ammo := mag_size
 var can_fire := true
 var is_reloading := false
 
+@onready var raycast := get_node(raycast_path) as RayCast3D
 @onready var fire_rate_timer := $FireRateTimer as Timer
 @onready var reload_timer := $ReloadTimer as Timer
 @onready var anim_player := $AnimationPlayer as AnimationPlayer
@@ -28,6 +30,7 @@ func _shoot() -> void:
 	print("Firing!")
 	can_fire = false
 	cur_ammo -= 1
+	_check_collision()
 	fire_rate_timer.start(fire_rate)
 	anim_player.play("firing")
 
@@ -39,6 +42,14 @@ func _reload() -> void:
 	is_reloading = true
 	reload_timer.start(reload_time)
 	anim_player.play("reloading")
+
+
+func _check_collision() -> void:
+	if raycast.is_colliding():
+		var collider := raycast.get_collider()
+		if collider.is_in_group("enemies"):
+			collider.queue_free()
+			print("Killed ", collider.name)
 
 
 func _on_fire_rate_timer_timeout() -> void:
--- scenes/world.gd
+++ scenes/world.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
 
-func _physics_process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
