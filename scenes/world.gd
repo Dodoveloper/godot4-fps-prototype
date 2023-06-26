423384e8c8b7d53203e37124ce35087c9aff653d
Dodoveloper <davide.cor94@gmail.com>
2023-06-26 08:50:11 +0200

    fix: decals on floor, ramp's collision

--- scenes/world.gd
+++ scenes/world.gd
@@ -3,6 +3,8 @@ extends Node3D
 
 const BULLET_DECAL := preload("res://scenes/bullet_decal/bullet_decal.tscn")
 
+@onready var decals_container := $Decals as Node
+
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -11,7 +13,11 @@ func _process(_delta: float) -> void:
 
 func _on_player_has_shot(raycast: RayCast3D) -> void:
 	var b := BULLET_DECAL.instantiate()
-	var collider := raycast.get_collider() as Node
-	collider.add_child(b)
+	raycast.get_collider().add_child(b)
 	b.global_transform.origin = raycast.get_collision_point()
-	b.look_at(b.global_transform.origin + raycast.get_collision_normal())
+	var collision_normal := raycast.get_collision_normal()
+	if collision_normal in [Vector3.UP, Vector3.DOWN]:
+		b.look_at(b.global_transform.origin + raycast.get_collision_normal(), Vector3.RIGHT)
+	else:
+		b.look_at(b.global_transform.origin + raycast.get_collision_normal(), Vector3.UP)
+	# TODO: keep the number of decals down
