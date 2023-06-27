6a409a0a156035534aff52ea88e0ca6f1d4b1623
Dodoveloper <davide.cor94@gmail.com>
2023-06-27 23:45:09 +0200

    feat: added weapon sway

--- entities/player/weapon.gd
+++ entities/player/weapon.gd
@@ -6,6 +6,8 @@ signal ads_toggled(enabled: bool)
 signal has_shot()
 
 const ADS_LERP_SPEED := 20
+const SWAY_TRESHOLD := 5
+const SWAY_LERP := 5
 
 @export var damage := 10
 @export var fire_rate := 0.08
@@ -17,6 +19,9 @@ const ADS_LERP_SPEED := 20
 @export var ads_position: Vector3
 @export var default_fov := 75.0
 @export var ads_fov := 55.0
+@export var sway_left: Vector3
+@export var sway_right: Vector3
+@export var sway_normal: Vector3
 
 var cur_ammo := mag_size:
 	set(value):
@@ -26,6 +31,7 @@ var can_fire := true
 var is_reloading := false
 var is_sprinting := false:
 	set = _set_is_sprinting
+var mouse_movement: float
 
 @onready var raycast := get_node(raycast_path) as RayCast3D
 @onready var camera := get_node(camera_path) as Camera3D
@@ -35,7 +41,21 @@ var is_sprinting := false:
 @onready var anim_player := $AnimationPlayer as AnimationPlayer
 
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		mouse_movement = -event.relative.x
+
+
 func _process(delta: float) -> void:
+	# sway
+	if mouse_movement != null:
+		if mouse_movement > SWAY_TRESHOLD:
+			rotation = rotation.lerp(sway_left, SWAY_LERP * delta)
+		elif mouse_movement < -SWAY_TRESHOLD:
+			rotation = rotation.lerp(sway_right, SWAY_LERP * delta)
+		else:
+			rotation = rotation.lerp(sway_normal, SWAY_LERP * delta)
+	# states
 	if not is_reloading:
 		if Input.is_action_pressed("ads"):
 			position = position.lerp(ads_position, ADS_LERP_SPEED * delta)
@@ -83,7 +103,7 @@ func _check_collision() -> void:
 func _set_is_sprinting(value: bool) -> void:
 	is_sprinting = value
 	can_fire = not is_sprinting
-	anim_player.play("sprinting" if is_sprinting else "RESET")
+	anim_player.play("sprinting" if is_sprinting else "holding")
 
 
 func _on_fire_rate_timer_timeout() -> void:
