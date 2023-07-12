3ac89b19eb9a662aed9c6d1da82a5e0a0f883018
Dodoveloper <davide.cor94@gmail.com>
2023-07-12 10:29:58 +0200

    refactor: reworked decals (WIP)

--- scenes/bullet_decal/bullet_decal.gd
+++ scenes/bullet_decal/bullet_decal.gd
@@ -6,5 +6,5 @@ const FADE_OUT_DURATION := 0.5
 
 func _on_life_timer_timeout() -> void:
 	var tween := create_tween()
-	tween.tween_property($MeshInstance3D, "transparency", 1.0, FADE_OUT_DURATION)
+	tween.tween_property(self, "modulate:a", 0.0, FADE_OUT_DURATION)
 	tween.tween_callback(queue_free)
--- scenes/world.gd
+++ scenes/world.gd
@@ -12,12 +12,8 @@ func _process(_delta: float) -> void:
 
 
 func _on_player_has_shot(raycast: RayCast3D) -> void:
-	var b := BULLET_DECAL.instantiate()
-	raycast.get_collider().add_child(b)
-	b.global_transform.origin = raycast.get_collision_point()
-	var collision_normal := raycast.get_collision_normal()
-	if collision_normal in [Vector3.UP, Vector3.DOWN]:
-		b.look_at(b.global_transform.origin + raycast.get_collision_normal(), Vector3.RIGHT)
-	else:
-		b.look_at(b.global_transform.origin + raycast.get_collision_normal(), Vector3.UP)
+	var decal := BULLET_DECAL.instantiate()
+	raycast.get_collider().add_child(decal)
+	decal.global_position = raycast.get_collision_point()
+	decal.look_at($Player/Head.global_rotation, raycast.get_collision_normal())
 	# TODO: keep the number of decals down
