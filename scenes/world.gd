72028b7e63ba906742ab444d71c5a4e8a296aa2b
Dodoveloper <davide.cor94@gmail.com>
2023-09-04 11:57:46 +0200

    feat: added doors

--- scenes/world.gd
+++ scenes/world.gd
@@ -6,6 +6,13 @@ const BULLET_DECAL := preload("res://scenes/bullet_decal/bullet_decal.tscn")
 @onready var decals_container := $Decals as Node
 
 
+#func _ready() -> void:
+#	await get_tree().create_timer(4.0).timeout
+#	$AnimationPlayer.play("open_door1")
+#	await get_tree().create_timer(10.0).timeout
+#	$AnimationPlayer.play("open_door2")
+
+
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
