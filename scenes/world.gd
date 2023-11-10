d1fc01568e97dde73c131882dc9cd0e056dab097
dodoveloper <davide.cor94@gmail.com>
2023-11-10 10:19:29 +0100

    fix: decals

--- scenes/world.gd
+++ scenes/world.gd
@@ -6,6 +6,7 @@ const BULLET_DECAL := preload("res://scenes/bullet_decal/bullet_decal.tscn")
 @onready var decals_container := $Decals as Node3D
 @onready var enemies_room1 := $FirstFloor/EnemiesRoom1 as Node3D
 @onready var enemies_room1_count := enemies_room1.get_child_count()
+@onready var anim_player := $AnimationPlayer as AnimationPlayer
 
 
 func _ready() -> void:
@@ -21,12 +22,14 @@ func _process(_delta: float) -> void:
 func _on_Enemy_destroyed() -> void:
 	enemies_room1_count -= 1
 	if enemies_room1_count == 0:
-		$AnimationPlayer.play("open_door1")
+		anim_player.play("open_door1")
 
 
 func _on_player_decal_requested(collision_info: Dictionary) -> void:
 	var decal := BULLET_DECAL.instantiate()
 	decals_container.add_child(decal)
 	decal.global_position = collision_info["position"]
-	decal.look_at(collision_info["normal"])
+	var collision_normal := collision_info["normal"] as Vector3
+	decal.look_at(decal.global_position + collision_normal,
+			Vector3.RIGHT if collision_normal in [Vector3.UP, Vector3.DOWN] else Vector3.UP)
 	# TODO: keep the number of decals down
